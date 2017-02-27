9e0da4daa691ceac9bd375d3d75baba222830aba
Sean Dunagan <dunagan5887@gmail.com>
2017-02-27 17:55:19 -0600

    Adding some comments to the method doc to give you some more direction

--- models/BaseballStats.php
+++ models/BaseballStats.php
@@ -14,6 +14,29 @@ class BaseballStats
      * To clarify if $cubs_team_stats = $baseballStatsModelObject->getTeamStats(), then $cubs_team_stats should be an
      * array. Every index of $cubs_team_stats should point to an array (an array which contains the stats for a player)
      *
+     * This method needs to return an array of the following form:
+     * array(
+     *      0 => array(
+     *          'Rk' => 1,
+     *          'Pos' => 'C',
+     *          'Name' => 'I'm a gay Cubs catcher lulz',
+     *          ...
+     *          ...
+ *              ...
+     *      ),
+     *      1 => array(
+     *          'Rk' => 2,
+     *          'Pos' => '1B',
+     *          'Name' => 'My name is Jizzer, but with an R instead of a J and an o instead of er'
+     *          ...
+     *          ...
+     *          ...
+     *      )
+     *      ...
+     *      ...
+     *      ...
+     * )
+     *
      * @return array
      */
     public function getTeamStats($connect)
