5be551b479b4c80be9fb3d0807308c24685bb8d4
Jeremy Hudspeth <jeremyhudspeth87@gmail.com>
2017-03-07 16:41:17 -0600

    updated

--- cubs.php
+++ cubs.php
@@ -2,56 +2,10 @@
 
 include 'models/BaseballStats.php';
 
-$connect = mysqli_connect("localhost", "root", "dellc123");
+
+
 
 // When Issue #2 is done, no code between here and END_REMOVE_FOR_2 should be in this file
-$result = mysqli_query($connect, "CREATE DATABASE cubs");
-$db = mysqli_select_db($connect, "cubs");
-define("CSV_PATH", "./data_files/");
-$csv_file = CSV_PATH . "cubs_2016_stats.csv";
-$trunc = "TRUNCATE TABLE IF EXISTS Players";
-
-$sql = "CREATE TABLE Players";
-if ($connect->query($sql) === TRUE){
-    echo "New record created successfully";
-} else {
-    echo "Error" . $sql . "<br>" . $connect->error;
-}
-
-if (($handle = fopen("CSV_PATH", "cubs_2016_stats.csv", "r")) !== FALSE) {
-    while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-
-        $sql = "INSERT INTO Players(`Rk`,`Pos`,`Name`,`Age`,`G`,`PA`,`AB`,`R`,`H`,`2B`,`3B`,`HR`,`RBI`,`SB`,`CS`,`BB`,`SO`,`BA`,`OBP`,`SLG`,`OPS`,`OPS+`,`TB`,`GDP`,`HBP`,`SH`,`SF`,`IBB`) VALUES(
-            '" . $row[0] . "',
-            '" . $row[1] . "',
-            '" . $row[2] . "',
-            '" . $row[3] . "',
-            '" . $row[4] . "',
-            '" . $row[5] . "',
-            '" . $row[6] . "',
-            '" . $row[7] . "',
-            '" . $row[8] . "',
-            '" . $row[9] . "',
-            '" . $row[10] . "',
-            '" . $row[11] . "',
-            '" . $row[12] . "',
-            '" . $row[13] . "',
-            '" . $row[14] . "',
-            '" . $row[15] . "',
-            '" . $row[16] . "',
-            '" . $row[17] . "',
-            '" . $row[18] . "',
-            '" . $row[19] . "',
-            '" . $row[20] . "',
-            '" . $row[21] . "',
-            '" . $row[22] . "',
-            '" . $row[23] . "',
-            '" . $row[24] . "',
-            '" . $row[25] . "',
-            '" . $row[26] . "',
-            '" . $row[27] . "')";}}
-
-
 // END_REMOVE_FOR_2
 ?>
 
